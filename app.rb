c2ab548d4203870b6d4400eb0371dfbaa9a5aac1
kevin lang <langkevina@gmail.com>
2019-08-28 23:45:02 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -23,6 +23,7 @@ class App < Sinatra::Base
     get '/say/:word1/:word2/:word3/:word4/:word5' do
       "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
     end
+
     get "/multiply/:num1/:num2" do
       @num1 = params[:num1].to_i
       @num2 = params[:num2].to_i
