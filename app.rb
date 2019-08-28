3593d09037f8c44816a672c84c41925dd8774c90
kevin lang <langkevina@gmail.com>
2019-08-28 23:50:51 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -12,7 +12,7 @@ class App < Sinatra::Base
     get "/square/:num" do
       # @user_name = params[:name]
       # "Goodbye, #{@user_name}."
-      val = params[:name].to_i
+      val = params[:num].to_i
       val = val * 2
       val.to_s
       # "#{@val}"
