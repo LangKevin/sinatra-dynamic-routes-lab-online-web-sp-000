636a8afca35dcfb023632ba3fecb57e8dbbefc1f
kevin lang <langkevina@gmail.com>
2019-08-28 23:53:06 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -25,7 +25,7 @@ class App < Sinatra::Base
     end
 
     get '/:operation/:number1/:number2' do
-       op = params[:operation]
+        op = params[:operation]
         val1 = params[:number1].to_i
         val2 = params[:number2].to_i
 
