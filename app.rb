a699e8545b792fc83e47e615cce557db37c49c43
kevin lang <langkevina@gmail.com>
2019-08-28 23:29:26 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,5 +2,15 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
+  get "/goodbye/:name" do
+      @user_name = params[:name]
+      "Goodbye, #{@user_name}."
+    end
 
+    get "/multiply/:num1/:num2" do
+      @num1 = params[:num1].to_i
+      @num2 = params[:num2].to_i
+      # @num1 * @num2
+       "#{@num1 * @num2}"
+    end
 end
